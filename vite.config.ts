c1647ffb6e8e9a6504e27a317d80e3f94fea2f4c
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-12-20 21:25:18 +0800

    chore: package and build and bundle

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom') || id.includes('react')) {
+              return 'react-vendor';
+            }
+            if (id.includes('openpgp')) {
+              return 'openpgp-vendor';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
