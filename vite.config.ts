eb77d48efa703e153239be4154c16219289a87a4
SessionHu <102411014+SessionHu@users.noreply.github.com>
2025-12-23 00:55:10 +0800

    chore: vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ viteReact() ],
+  clearScreen: false,
   build: {
+    license: {
+      fileName: 'LICENSE.thirdparty.md'
+    },
     rollupOptions: {
       output: {
         manualChunks(id: string) {
           if (id.includes('node_modules')) {
             if (id.includes('react-dom') || id.includes('react')) {
-              return 'react-vendor';
+              return 'react';
             }
             if (id.includes('openpgp')) {
-              return 'openpgp-vendor';
+              return 'openpgp';
             }
             return 'vendor';
           }
         },
       },
     },
+    sourcemap: true,
   },
 });
